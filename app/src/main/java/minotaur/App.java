b646990ef2dec15c0bc9839a1427a0267a8269b7
Brandon Rubio <brubio3724@gmail.com>
2023-02-17 13:35:54 -0500

    Added user input

--- app/src/main/java/minotaur/App.java
+++ app/src/main/java/minotaur/App.java
@@ -1,13 +1,30 @@
 package minotaur;
 
+import java.util.Scanner;
+
 import minotaur.one.Birthday;
 import minotaur.two.CrystalVase;
 
 public class App {
     public static void main(String[] args) {
-        // Birthday birthday = new Birthday(100);
-        // birthday.simulate();
-        CrystalVase crystalVase = new CrystalVase(5);
-        crystalVase.simulate();
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        scan.close();
+
+        String[] parts = input.split(" ");
+
+        String problem = parts[0];
+        Integer guestCount = Integer.parseInt(parts[1]);
+        System.out.println(problem + " " + guestCount);
+
+        if (problem.equals("birthday")) {
+            Birthday birthday = new Birthday(guestCount);
+            birthday.simulate();
+        } else if (problem.equals("vase")) {
+            CrystalVase crystalVase = new CrystalVase(guestCount);
+            crystalVase.simulate();
+        } else {
+            System.out.println("Invalid input");
+        }
     }
 }
